80e533412794c8691715325aa0eb64bbfc718406
asma ajmi <114587566+asmaajmi@users.noreply.github.com>
2026-03-06 13:20:23 +0100

    Rename users table to user in init.sql

--- database/init.sql
+++ database/init.sql
@@ -1,9 +1,9 @@
-CREATE TABLE users (
+CREATE TABLE user (
   id SERIAL PRIMARY KEY,
   name VARCHAR(100) NOT NULL,
   email VARCHAR(100) UNIQUE NOT NULL
 );
 
-INSERT INTO users (name, email) VALUES
+INSERT INTO user (name, email) VALUES
 ('Alice', 'alice@example.com'),
 ('Bob', 'bob@example.com');
